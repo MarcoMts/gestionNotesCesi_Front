5d5794e5c5f928784e67a2e1ff9626eca8eab9a6
Marco Martins <marcomartinsdev@gmail.com>
2018-03-05 13:52:27 +0100

    class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
-
+import { ConsultationComponent } from './consultation/consultation.component';
+import { OptionsComponent } from './options/options.component';
+import { RouterModule, Routes} from '@angular/router';
+import {NoteService} from './note.service';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { ConsultationPiloteComponent } from './consultation-pilote/consultation-pilote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsultationComponent,
+    OptionsComponent,
+    LoginComponent,
+    HeaderComponent,
+    ConsultationPiloteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {path:'consultation',component:ConsultationComponent},
+      {path:'consultationPilote',component:ConsultationPiloteComponent},
+      {path:'options',component:OptionsComponent}
+      
+    ])
   ],
-  providers: [],
+  providers: [NoteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
